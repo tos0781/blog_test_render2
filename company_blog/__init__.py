5f865ec9196eb0f96e55ef1496a4b79a85642c7d
Toshirou <jelemy84@gmail.com>
2026-07-01 07:33:46 +0900

    fix create_all

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -36,6 +36,12 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
+
+# ✅ これ追加
+with app.app_context():
+    db.create_all()
+
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "users.login"
